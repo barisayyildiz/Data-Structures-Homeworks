8de330de531ea34a06ae25c6e69bc74740e36056
barisayyildiz <barisayyildiz.1799@gmail.com>
2021-04-24 20:18:06 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import com.*;
+
+public class Main
+{
+	public static void main(String args[])
+	{
+		Heap<Integer> h1 = new Heap<Integer>();
+		h1.offer(3);
+		h1.offer(7);
+		h1.offer(2);
+		h1.offer(4);
+		h1.offer(1);
+
+		System.out.println(h1);
+		System.out.println(h1.removeKthLargest(4));
+
+		h1.offer(10);
+		h1.offer(2);
+		System.out.println(h1);
+
+	}
+}
+
