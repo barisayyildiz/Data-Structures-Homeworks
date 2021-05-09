db3d3f9151f2cd3ddbffb47bbf639871be157478
barisayyildiz <barisayyildiz.1799@gmail.com>
2021-05-09 14:59:52 +0300

    part1 yapıldı

--- Main.java
+++ Main.java
@@ -21,32 +21,50 @@ public class Main
 
 		// System.out.println(mapIter);
 
-		HashMapOpen<Integer,String> hmap = new HashMapOpen<Integer,String>();
 
-		hmap.put(1,"baris");
-		hmap.put(2,"baqwes");
-		hmap.put(12,"!!!!!!!!!!!!!!");
-		hmap.put(3,"bqweqw");
-		hmap.put(4,"b12312");
-		hmap.put(5,"23gfsf");
-		hmap.put(6,"rgwe");
-		hmap.put(7,"b2qfqws");
-		hmap.put(11,"!!!!!");
-		hmap.put(67,"bqweqw");
-		hmap.put(13,"b12312");
-		hmap.put(34,"23gfsf");
-		// hmap.put(-3,"b2qfqws");
 
-		System.out.println(hmap);
 
-		hmap.remove(7);
-		hmap.remove(6);
-		hmap.remove(12);
-		hmap.remove(1);
-		hmap.remove(2);
+		// HashMapOpen<Integer,String> hmap = new HashMapOpen<Integer,String>();
 
-		// System.out.println(hmap.get(12));
-		System.out.println(hmap);
+		// hmap.put(1,"baris");
+		// hmap.put(2,"baqwes");
+		// hmap.put(12,"!!!!!!!!!!!!!!");
+		// hmap.put(3,"bqweqw");
+		// hmap.put(4,"b12312");
+		// hmap.put(5,"23gfsf");
+		// hmap.put(6,"rgwe");
+		// hmap.put(7,"b2qfqws");
+		// hmap.put(11,"!!!!!");
+		// hmap.put(67,"bqweqw");
+		// hmap.put(13,"b12312");
+		// hmap.put(34,"23gfsf");
+		// // hmap.put(-3,"b2qfqws");
+
+		// System.out.println(hmap);
+
+		// hmap.remove(7);
+		// hmap.remove(6);
+		// hmap.remove(12);
+		// hmap.remove(1);
+		// hmap.remove(2);
+
+		// // System.out.println(hmap.get(12));
+		// System.out.println(hmap);
+
+
+		MapIterator<Integer,String> mapiter = new MapIterator<Integer,String>();
+
+		mapiter.put(2,"baris");
+		mapiter.put(6,"asya");
+		mapiter.put(21,"ruzgar");
+
+		mapiter.next();
+		mapiter.next();
+		mapiter.prev();
+
+		while(mapiter.hasNext()){
+			System.out.println(mapiter.next());
+		}
 
 
 
--- com/MapIterator.java
+++ com/MapIterator.java
@@ -5,27 +5,60 @@ import java.util.HashMap;
 public class MapIterator<K,V> extends HashMap<K,V>
 {
 	private int counter;
+	private K[] keyArr;
+	private K initialKey;
+	private boolean setInitial;
 
 	public MapIterator(){
 		super();
 		this.counter = 0;
+		this.setKeyArray();
+
+		this.setInitial = false;
+	}
+
+	public MapIterator(K key){
+		super();
+		this.counter = 0;
+		this.setKeyArray();
+
+		this.initialKey = key;
+		this.setInitial = true;
+
+	}
+
+	@SuppressWarnings("unchecked")
+	private void setKeyArray(){
+		this.keyArr = (K[])keySet().toArray();
+		
+		if(this.setInitial){
+			K temp;
+			for(int i=0; i<size(); i++){
+				if(this.keyArr[i].equals(this.initialKey)){
+					// swap the values
+					temp = this.keyArr[0];
+					this.keyArr[0] = this.initialKey;
+					this.keyArr[i] = temp;
+					break;
+				}
+			}
+		}
+
 	}
 
 	public boolean hasNext(){
 		return this.counter < size();
 	}
 
-	@SuppressWarnings("unchecked")
 	public K next(){
-		K[] temp = (K[])keySet().toArray();
-		return temp[this.counter++];
+		this.setKeyArray();
+		return this.keyArr[this.counter++];
 	}
-
-	@SuppressWarnings("unchecked")
-	public K prev(){
-		K[] temp = (K[])keySet().toArray();
-		return temp[--this.counter];
-	}
-
 	
+	public K prev(){
+		this.setKeyArray();
+		return this.keyArr[--this.counter];
+	}
+
 }
+
