2f57371339289975ed4d35936ec125ae1ad9d218
barisayyildiz <barisayyildiz.1799@gmail.com>
2021-05-09 22:38:08 +0300

    update on test file

--- Main.java
+++ Main.java
@@ -31,7 +31,8 @@ public class Main
 			System.out.println(hashMapIter2.next());
 		}
 
-		// ============= Test Cases for HashMapList
+		System.out.println("\n ================================== Test Cases for HashMaps ================================== \n");
+
 		// ============= Small Sized Hash Maps
 		HashMapList<Integer,String> hMapList = new HashMapList<Integer,String>();
 		HashMapTree<Integer,String> hMapTree = new HashMapTree<Integer,String>();
@@ -43,18 +44,12 @@ public class Main
 			rndInt = randomIntger(20);
 			rndStr = randomString();
 
-			System.out.println("rand : " + rndInt + ", str : " + rndStr);
-
 			hMapList.put(rndInt, rndStr);
 			hMapTree.put(rndInt, rndStr);
 			hMapOpen.put(rndInt, rndStr);
 		}
 
-		System.out.println("HashMapList : " +  hMapList);
-		System.out.println("HashMapTree : " +  hMapTree);
-		System.out.println("HashMapOpen : " +  hMapOpen + "\n");
-
-		System.out.println("For HashMapList key : " + rndInt + ", value : " + hMapList.get(rndInt));
+		System.out.println("\nFor HashMapList key : " + rndInt + ", value : " + hMapList.get(rndInt));
 		System.out.println("For HashMapTree key : " + rndInt + ", value : " + hMapTree.get(rndInt));
 		System.out.println("For HashMapOpen key : " + rndInt + ", value : " + hMapOpen.get(rndInt));
 
@@ -86,12 +81,8 @@ public class Main
 			hMapOpen.put(rndInt, rndStr);
 		}
 
-		System.out.println("HashMapList : " +  hMapList + "\n");
-		System.out.println("HashMapTree : " +  hMapTree + "\n");
-		System.out.println("HashMapOpen : " +  hMapOpen + "\n");
 
-
-		System.out.println("For HashMapList key : " + rndInt + ", value : " + hMapList.get(rndInt));
+		System.out.println("\nFor HashMapList key : " + rndInt + ", value : " + hMapList.get(rndInt));
 		System.out.println("For HashMapTree key : " + rndInt + ", value : " + hMapTree.get(rndInt));
 		System.out.println("For HashMapOpen key : " + rndInt + ", value : " + hMapOpen.get(rndInt));
 
@@ -103,9 +94,6 @@ public class Main
 		System.out.println("For HashMapList key : " + rndInt + ", value : " + hMapList.get(rndInt));
 		System.out.println("For HashMapTree key : " + rndInt + ", value : " + hMapTree.get(rndInt));
 		System.out.println("For HashMapOpen key : " + rndInt + ", value : " + hMapOpen.get(rndInt));
-
-
-
 		
 
 		System.out.println("\n =============== Adding 500 more items =============== \n");
@@ -120,10 +108,7 @@ public class Main
 			hMapOpen.put(rndInt, rndStr);
 		}
 
-		System.out.println("HashMapList : " +  hMapList + "\n");
-		System.out.println("HashMapTree : " +  hMapTree + "\n");
 		System.out.println("HashMapOpen : " +  hMapOpen + "\n");
-
 
 		System.out.println("For HashMapList key : " + rndInt + ", value : " + hMapList.get(rndInt));
 		System.out.println("For HashMapTree key : " + rndInt + ", value : " + hMapTree.get(rndInt));
@@ -163,134 +148,9 @@ public class Main
 		return rand.nextInt(n);
 	}
 
-
-	public static void testCases2(){
-
-		HashMapOpen<Integer,String> hMapOpen = new HashMapOpen<Integer,String>();
-		hMapOpen.put(17,"qawe");
-		hMapOpen.put(17,"qawe");
-		hMapOpen.put(13,"qawe");
-		hMapOpen.put(8,"qawe");
-		hMapOpen.put(14,"qawe");
-		hMapOpen.put(2,"qawe");
-		hMapOpen.put(11,"qawe");
-		hMapOpen.put(12,"qawe");
-		hMapOpen.put(2,"qawe");
-		hMapOpen.put(16,"qawe");
-		hMapOpen.put(0,"qawe");
-		hMapOpen.put(3,"qawe");
-		hMapOpen.put(5,"qawe");
-		hMapOpen.put(6,"qawe");
-		hMapOpen.put(7,"qawe");
-		hMapOpen.put(10,"qawe");
-		hMapOpen.put(16,"qawe");
-		hMapOpen.put(18,"qawe");
-		hMapOpen.put(6,"qawe");
-		hMapOpen.put(11,"qawe");
-
-		System.out.println(hMapOpen);
-
-	}
-
 	public static void main(String args[]){
 
-		// testCases2();
-
 		testCases();
-
-		// HashMapList<Integer,String> hmap = new HashMapList<Integer,String>();
-		// hmap.put(3,"baris");
-		// hmap.put(4,"asya");
-
-		// hmap.put(3,"pelin");
-
-		// System.out.println(hmap);
-
-		// // MapIterator<String,String> mapIter = new MapIterator<String,String>();
-
-		// // mapIter.put("56941479184","baris");
-		// // mapIter.put("55555555555","john");
-		// // mapIter.put("33333333333","doe");
-
-		// // mapIter.next();
-		// // mapIter.next();
-		// // mapIter.prev();
-
-		// // while(mapIter.hasNext()){
-		// // 	System.out.println(mapIter.next());
-		// // }
-
-		// // System.out.println(mapIter);
-
-
-
-
-		// HashMapList<Integer,String> hmap = new HashMapList<Integer,String>();
-
-		// hmap.put(1,"asd");
-		// hmap.put(2,"qwe");
-		// hmap.put(3,"qwe");
-		// hmap.put(4,"qwe");
-		// hmap.put(5,"qwe");
-		// hmap.put(6,"qwe");
-		// hmap.put(7,"qwe");
-		// hmap.put(8,"qwe");
-		// hmap.put(8,"123213");
-		// // hmap.put(9,"qwe");
-		// // hmap.put(10,"qwe");
-		// // hmap.put(11,"qwe");
-		// // hmap.put(12,"qwe");
-		// // hmap.put(13,"qwe");
-		// // hmap.put(14,"qwe");
-		// // hmap.put(15,"qwe");
-		// // hmap.put(16,"qwe");
-
-		// System.out.println(hmap);
-
-		// hmap.remove(4);
-
-		// // hmap.put(1,"baris");
-		// // hmap.put(2,"baqwes");
-		// // hmap.put(12,"!!!!!!!!!!!!!!");
-		// // hmap.put(3,"bqweqw");
-		// // hmap.put(4,"b12312");
-		// // hmap.put(5,"23gfsf");
-		// // // hmap.put(12,"qqqqqqqqqqqqqqqqqqqqqqqq");
-		// // hmap.put(6,"rgwe");
-		// // hmap.put(7,"b2qfqws");
-		// // hmap.put(11,"!!!!!");
-		// // hmap.put(67,"bqweqw");
-		// // hmap.put(13,"b12312");
-		// // hmap.put(34,"23gfsf");
-		// // // hmap.put(-3,"b2qfqws");
-
-		// // System.out.println(hmap);
-
-		// // hmap.remove(7);
-		// // hmap.remove(6);
-		// // hmap.remove(12);
-		// // hmap.remove(1);
-		// // hmap.remove(2);
-
-		// // System.out.println(hmap.get(4));
-		// System.out.println(hmap);
-
-
-		// // MapIterator<Integer,String> mapiter = new MapIterator<Integer,String>();
-
-		// // mapiter.put(2,"baris");
-		// // mapiter.put(6,"asya");
-		// // mapiter.put(21,"ruzgar");
-
-		// // mapiter.next();
-		// // mapiter.next();
-		// // mapiter.prev();
-
-		// // while(mapiter.hasNext()){
-		// // 	System.out.println(mapiter.next());
-		// // }
-
-
 
 	}
 }
